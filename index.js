b5b6e326932419311cee6e1b7daa1b65bcf635b1
Jesus Cereceres <jesus.edelcereceres@gmail.com>
2016-09-05 18:11:08 -0500

    the doc is added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict'
+// you pass the mode of every call from BPW_MODE, set to PRD
+// to run in mode production, default mode is RND
 describe('Payworks#auth', function () {
     require('./testing/auth')
     require('./testing/reverse')

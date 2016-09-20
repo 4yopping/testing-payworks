14d0e00e4b643abb92526643e84e34643a2595f1
Jesus Cereceres <jesus.edelcereceres@gmail.com>
2016-09-20 14:34:32 -0500

    the reverse orden is modified

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ describe('Payworks#auth', function () {
         }
     });
     require('./testing/auth')
+    require('./testing/refund')
     require('./testing/reverse')
     require('./testing/cancelation')
     require('./testing/preAuth')
     require('./testing/postAuth')
-    require('./testing/refund')
     require('./testing/verify')
 })
--- testing/refund.js
+++ testing/refund.js
@@ -1,7 +1,5 @@
 'use strict'
 it('should obtain a refund', function * () {
-    this.params = Object.assign(this.data,{control_number: this.controlNumber()} )
-  this.payment = yield this.payworks.auth(this.params)
   this.params = {
     amount: this.data.amount,
     reference: this.payment.referencia
